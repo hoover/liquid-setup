16e3fc4e322e1613c959bb24b8da3548337ee0f5
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-06-19 18:49:58 +0300

    box the first vm and use it as base for the others

---
+++ test/zeroconf/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+set -e
+
+#vagrant destroy one
+#vagrant up one --provision
+#vagrant halt one
+
+# delete the package.box if exists
+#[ -e package.box ] && rm -rf package.box
+
+# package the box
+#vagrant package one
+
+# add the box into vagrant
+vagrant box add liquid-zeroconf ./package.box --force
+
+# up the rest of the vms
+#vagrant up two
+#vagrant up three
+vagrant up
