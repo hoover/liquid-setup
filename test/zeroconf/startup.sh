e25d69d8948329eb9eb642180317ae4d39d13b24
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-06-20 19:03:07 +0300

    change class of internal ip addresses

--- test/zeroconf/startup.sh
+++ test/zeroconf/startup.sh
@@ -2,20 +2,18 @@
 set -x
 set -e
 
-#vagrant destroy one
-#vagrant up one --provision
-#vagrant halt one
+vagrant destroy -f
+vagrant up one --provision
+vagrant halt one
 
 # delete the package.box if exists
-#[ -e package.box ] && rm -rf package.box
+[ -e package.box ] && rm -rf package.box
 
 # package the box
-#vagrant package one
+vagrant package one
 
 # add the box into vagrant
 vagrant box add liquid-zeroconf ./package.box --force
 
 # up the rest of the vms
-#vagrant up two
-#vagrant up three
-vagrant up
+vagrant up --provision
