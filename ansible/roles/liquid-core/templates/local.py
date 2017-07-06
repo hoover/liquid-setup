5a9425af075c7fe09629b4ac987cd3c55d991209
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-07-06 19:20:13 +0300

    don't put localhost in liquid-core allowed hosts

--- ansible/roles/liquid-core/templates/local.py
+++ ansible/roles/liquid-core/templates/local.py
@@ -1,6 +1,6 @@
 SECRET_KEY = '-- secret key --'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '{{ liquid_domain }}']
+ALLOWED_HOSTS = ['{{ liquid_domain }}']
 AUTH_PASSWORD_VALIDATORS = []
 CORS_ORIGIN_ALLOW_ALL = True
 
